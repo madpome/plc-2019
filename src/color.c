e1c012c867aee281de9b1384fc4091d5e10c0c53
Philippe EAR <earphilippefr@gmail.com>
2019-05-21 14:40:01 +0200

    struct color pour conversion YCbCr to RGB

---
+++ src/color.c
@@ -0,0 +1,23 @@
+
+typedef struct RGB{
+    uint8_t R;
+    uint8_t G;
+    uint8_t B;
+}RGB;
+
+
+public RGB ycbcr_to_rgb(float Y, float C_r, float C_b){
+    RGB rgb;
+    int a = Y + 1.402*(C_r-128);
+    a = (r<0)?0:a;
+    a = (r>255)?255:a;
+    rgb.R = (uint_8t) a;
+    a = Y - 0.34414*(C_b - 128) - 0.71313*(C_r - 128);
+    a = (r<0)?0:a;
+    a = (r>255)?255:a;
+    rgb.G = (uint_8t) a;
+    a = Y + 1.772*(C_b - 128);
+    a = (r<0)?0:a;
+    a = (r>255)?255:a;
+    rgb.B = (uint8_t ) a;
+}
