d53f283f0fa2bb7c0b16b6cbb5a641809c5ccc5e
Philippe EAR <earphilippefr@gmail.com>
2019-05-22 22:45:29 +0200

    version naive de idct

--- src/idct.c
+++ src/idct.c
@@ -1,6 +1,34 @@
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+#define C(x) ((x==0)?(1/sqrt(2)):1)
+// A COMPILER AVEC -lm
+float ** naive_idct(int32_t **frequence, float ** table_cos){
+    float **tab = malloc(8*sizeof(float*));
+    for(int i =0;i<8;i++){
+        tab[i] = malloc(8*sizeof(float));
+    }
+    for(int i =0;i<8;i++){
+        for(int j = 0;j<8;j++){
+            for(int lambda =0;lambda<8;lambda++){
+                for(int gamma = 0;gamma<8;gamma++){
+                    tab[i][j]+=C(lambda)*C(gamma)*table_cos[i][lambda]*table_cos[j][gamma]*frequence[lambda][gamma];
+                }
+            }
+        }
+    }
+    return tab;
+}
 
-
-
-float ** naive_idct(int32_t **frequence){
-    
+int main(int argc, char ** argv){
+    float **table_cos = malloc(8*sizeof(float*));
+    for(int i =0;i<8;i++){
+        table_cos[i] = malloc(8*sizeof(float));
+    }
+    for(int i =0;i<8;i++){
+        for(int j = 0;j<8;j++){
+            //table_cos[i][j] = cos((2i+1)*j*pi/16)
+            table_cos[i][j] = cos((2*i+1)*j*M_PI/16);
+        }
+    }
 }
