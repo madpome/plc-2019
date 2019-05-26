2e85eb237acf951b727dbc29a1dec09f6731f80b
Philippe EAR <earphilippefr@gmail.com>
2019-05-26 16:42:57 +0200

    correction indentation et tout jpeg2ppm,fast idct impletenté

--- include/idct.h
+++ include/idct.h
@@ -7,5 +7,7 @@
 
 extern float ** naive_idct(int16_t **fequence, float ** table_cos);
 extern float ** cos_table();
+extern float ** idct(int16_t **frequence);
+extern float* idct_colonne(float **frequence, int col);
+extern float* idct_ligne(float* frequence, int16_t *freq_true, int free_it);
 #endif
-
