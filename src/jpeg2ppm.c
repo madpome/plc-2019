f646c913390784ec2470b1b31eef947328e6b2ed
Quentin Sarrazin <Quentin.Sarrazin@grenoble-inp.org>
2019-05-28 10:10:36 +0200

    bordures chlagoss

--- src/jpeg2ppm.c
+++ src/jpeg2ppm.c
@@ -28,8 +28,15 @@ int main(int argc, char **argv){
     printf("%d\n", get_nb_quantization_tables(jpeg));
     uint16_t horizontal = get_image_size(jpeg,DIR_H);
     uint16_t vertical = get_image_size(jpeg,DIR_V);
-    uint16_t nb_bloc_h = horizontal/8;
-    uint16_t nb_bloc_v = vertical/8;
+    uint16_t nb_bloc_h = horizontal/8+1;
+    uint16_t nb_bloc_v = vertical/8+1;
+
+
+    printf("%i\n", nb_bloc_h);
+    printf("%i\n", nb_bloc_v);
+    printf("%i\n", horizontal);
+    printf("%i\n", vertical);
+
 
     /* Création de l'image avec les blocs traduits */
     int16_t ***image = trad_image(stream, jpeg,nb_bloc_h,nb_bloc_v);
@@ -59,12 +66,16 @@ int main(int argc, char **argv){
     }
 
     /* On transforme le tableau de blocs en image de pixels */
-    struct RGB **immondice = bloc2array(image_gris,nb_bloc_h,nb_bloc_v, horizontal, vertical);
+    struct RGB **immondice = bloc2array(image_gris,nb_bloc_h,nb_bloc_v, 8*nb_bloc_h, 8*nb_bloc_v);
     pixels_to_ppm(immondice,horizontal,vertical,1,argv[1]);
+
+
+    /* LIBERATION */
     for(int i =0;i<vertical;i++){
         free(immondice[i]);
     }
     free(immondice);
+
     for(int i =0;i<nb_bloc_v;i++){
         for(int j=0;j<nb_bloc_h;j++){
             for(int a = 0;a<8;a++){
